db3a3e0af09eb9b9633c8b6966aa150df7269b54
AD <95749329+Ariboux@users.noreply.github.com>
2024-02-05 00:26:13 +0100

    issue resolved

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import os
 import pyfiglet
 import readchar
+import re
 
 class Task:
-    def __init__(self, text):
+    def __init__(self, text, deadline=None):
         self.text = text
+        self.deadline = deadline
         self.validate = False
     
     def validate_task(self):
@@ -17,24 +19,30 @@ class ToDoList:
 
     def add_task(self, task):
         self.tasks.append(task)
-        print(f"Task added: {task.text}, validate: {task.validate}")
+        if task.deadline:
+            print(f"Task added: {task.text} with deadline: {task.deadline}, Validate: {task.validate}")
+        else:
+            print(f"Task added: {task.text}, Validate: {task.validate}")
 
     def view_tasks(self):
         if not self.tasks:
             print("No tasks in the list.")
             return
         for i, task in enumerate(self.tasks, 1):
-            print(f"{i}. {task.text}, validate: {task.validate}")
+            if task.deadline:
+                print(f"{i}. {task.text}, Deadline: {task.deadline}, Validate: {task.validate}")
+            else:
+                print(f"{i}. {task.text}, Validate: {task.validate}")
 
     def remove_task(self, index):
-        if index - 1 < len(self.tasks):
+        if 0 <= index - 1 < len(self.tasks):
             removed_task = self.tasks.pop(index - 1)
             print(f"Removed task: {removed_task.text}")
         else:
             print("Invalid task number.")
 
     def validate_task_by_index(self, index):
-        if index - 1 < len(self.tasks):
+        if 0 <= index - 1 < len(self.tasks):
             validate_task = self.tasks[index - 1].validate_task()
             print(f"The task: '{validate_task.text}' has been validated")
         else:
@@ -77,7 +85,19 @@ def main():
             if current_option == 0:
                 clear_screen()
                 task_text = input("Enter a task: ")
-                todo_list.add_task(Task(task_text))
+                while not task_text:
+                    task_text = input("You have to name the task.\nEnter a task: ")
+                
+                def is_valid_date(date):
+                    pattern = r"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/((19|20)\d\d)$"
+                    return re.match(pattern, date) is not None
+
+                deadline = input("Enter a deadline (dd/mm/yyyy) (OPTIONAL): ")
+                while deadline and not is_valid_date(deadline):
+                    print("Invalid date format. Please enter a valid deadline in the format dd/mm/yyyy.")
+                    deadline = input("Enter a deadline (optional): ")
+
+                todo_list.add_task(Task(task_text, deadline))
                 readchar.readkey()
             elif current_option == 1:
                 clear_screen()
